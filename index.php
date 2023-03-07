1da96b77d6d95a67f2ba0ca1ba18a910d9196952
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-07 03:10:01 +0000

    Added PHP

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+
+    <!-- Meta data -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Web page title -->
     <title>Hello, World! in PHP</title>
 	</head>
 	<body>
+
+    <!-- Web page content -->
     <h1><?php echo 'Hello, World! in PHP'; ?></h1>
 		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
 	</body>
