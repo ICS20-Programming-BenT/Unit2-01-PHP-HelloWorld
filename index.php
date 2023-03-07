4353f67515930d0b21a298c8dae422fa3e4d4153
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-07 15:59:29 +0000

    Added style

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="./css/style.css">
+
     <!-- Web page title -->
     <title>Hello, World! in PHP</title>
 	</head>
 	<body>
 
     <!-- Web page content -->
-    <h1><?php echo 'Hello, World! in PHP'; ?></h1>
-		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
+    <h1><?php echo 'There is something special about this web page...'; ?></h1>
+		<?php echo '<p>This is my first web page using PHP! I enjoyed creating this web page using an entirely new langauge, but one that I could use my knowledge of HTML to improve.</p>'; ?>
 	</body>
 </html>
